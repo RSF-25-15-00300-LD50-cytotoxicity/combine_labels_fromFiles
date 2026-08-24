dd409a100a4428439b0d6accea80093df9cce361
pavelVPo <97514993+pavelVPo@users.noreply.github.com>
2026-08-24 18:33:52 +0300

    Add files via upload

---
+++ gather_everything__Rcode.R
@@ -0,0 +1,13 @@
+# List files from AAL
+
+# Calculate descriptors to be able to merge the data according to the PASS internal representation
+
+# Read the files and parse to get the access to structures, descriptors and labels
+
+# Combine the data from all files to the single SDF
+
+# Further processing (???)
+
+# Convert structures to SMILES using CDK for R (???)
+
+# Export the resulting SDF and CSV / TAB (???)
